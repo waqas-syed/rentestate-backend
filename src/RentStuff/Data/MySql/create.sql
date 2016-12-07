bd1c1d6975c7d05d5fff38727f71eb366134f2f2
waqas-syed <waqas.shah.syed@gmail.com>
2016-12-07 10:52:01 +0500

    Added Property Type table

--- src/RentStuff/Data/MySql/create.sql
+++ src/RentStuff/Data/MySql/create.sql
@@ -18,7 +18,7 @@
 --
 -- Table structure for table `dimension`
 --
-USE rentstuff;
+
 DROP TABLE IF EXISTS `dimension`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
@@ -99,6 +99,20 @@ CREATE TABLE `house` (
   UNIQUE KEY `id_UNIQUE` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `propertytype`
+--
+
+DROP TABLE IF EXISTS `propertytype`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `propertytype` (
+  `property_id` int(11) NOT NULL AUTO_INCREMENT,
+  `property_type` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`property_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -109,4 +123,4 @@ CREATE TABLE `house` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-09  1:58:20
+-- Dump completed on 2016-12-07 10:50:43
--- src/RentStuff/Data/MySql/populate.sql
+++ src/RentStuff/Data/MySql/populate.sql
@@ -18,7 +18,7 @@
 --
 -- Dumping data for table `dimension`
 --
-USE rentstuff;
+
 LOCK TABLES `dimension` WRITE;
 /*!40000 ALTER TABLE `dimension` DISABLE KEYS */;
 INSERT INTO `dimension` VALUES ('0f443ce9-f84b-442e-9922-bb16cf88c4ea','1',NULL,5.0000),('2a3eeb36-9a28-43fe-897e-5d03bcfdb074','1',NULL,5.0000),('2c2d0206-bb79-4895-b842-2575613bdb2b','1',NULL,5.0000);
@@ -43,6 +43,16 @@ LOCK TABLES `house` WRITE;
 INSERT INTO `house` VALUES ('2fb0fbbc-7f61-49eb-b157-51d766d31065',6,'bilbo@baggins.com',50000,5,0,0,1,0,1,0,0,0,0,'0',0,'1234567890',33.649794,73.068567,'747/A','13','Pindora, Rawalpindi, Pakistan','0f443ce9-f84b-442e-9922-bb16cf88c4ea'),('551d5c06-714e-4533-9f8b-9694a3e7e5ed',7,'bilbo@baggins.com',50000,5,0,0,1,0,1,0,0,0,0,'0',0,'1234567890',33.649794,73.068567,'747/A','13','Pindora, Rawalpindi, Pakistan','2a3eeb36-9a28-43fe-897e-5d03bcfdb074'),('8eebd019-6a71-4909-9efe-0802a510ab71',8,'bilbo@baggins.com',50000,5,0,0,1,0,1,0,0,0,0,'0',0,'1234567890',33.649794,73.068567,'747/A','13','Pindora, Rawalpindi, Pakistan','2c2d0206-bb79-4895-b842-2575613bdb2b');
 /*!40000 ALTER TABLE `house` ENABLE KEYS */;
 UNLOCK TABLES;
+
+--
+-- Dumping data for table `propertytype`
+--
+
+LOCK TABLES `propertytype` WRITE;
+/*!40000 ALTER TABLE `propertytype` DISABLE KEYS */;
+INSERT INTO `propertytype` VALUES (1,'House'),(2,'Apartment'),(3,'Hostel'),(4,'Hotel');
+/*!40000 ALTER TABLE `propertytype` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -53,4 +63,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-06-09  1:58:52
+-- Dump completed on 2016-12-07 10:51:11
