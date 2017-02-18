1d184f8030118a2621e1214a10a3ce980b9e6353
waqas-syed <waqas.shah.syed@gmail.com>
2017-02-18 15:40:54 +0500

    OAuth Authorization Bearer Token: Working

--- src/RentStuff/IdentityAndAccess/Ports/RentStuff.IdentityAndAccess.Adapter.Rest/Startup.cs
+++ src/RentStuff/IdentityAndAccess/Ports/RentStuff.IdentityAndAccess.Adapter.Rest/Startup.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Http;
 using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
 using Owin;
+using RentStuff.IdentityAndAccess.Adapter.Rest.Providers;
 
 [assembly: OwinStartup(typeof(RentStuff.IdentityAndAccess.Adapter.Rest.Startup))]
 namespace RentStuff.IdentityAndAccess.Adapter.Rest
@@ -14,8 +16,26 @@ namespace RentStuff.IdentityAndAccess.Adapter.Rest
         public void Configuration(IAppBuilder app)
         {
             HttpConfiguration config = new HttpConfiguration();
+            ConfigureOAuth(app);
             WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);
+        }
+
+        private void ConfigureOAuth(IAppBuilder app)
+        {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
+            {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new SimpleAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
