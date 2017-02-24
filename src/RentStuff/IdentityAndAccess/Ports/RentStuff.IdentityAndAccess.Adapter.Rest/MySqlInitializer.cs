3447c3ac9cc8ec423df3a0f9b6c242614a841d86
waqas-syed <waqas.shah.syed@gmail.com>
2017-02-24 10:14:06 +0500

    Database not created everytime the program runs

--- src/RentStuff/IdentityAndAccess/Ports/RentStuff.IdentityAndAccess.Adapter.Rest/MySqlInitializer.cs
+++ src/RentStuff/IdentityAndAccess/Ports/RentStuff.IdentityAndAccess.Adapter.Rest/MySqlInitializer.cs
@@ -22,7 +22,7 @@ namespace RentStuff.IdentityAndAccess.Adapter.Rest
                 var migrationHistoryTableExists = ((IObjectContextAdapter)context).ObjectContext.ExecuteStoreQuery<int>(
                 string.Format(
                   "SELECT COUNT(*) FROM information_schema.tables WHERE table_schema = '{0}' AND table_name = '__MigrationHistory'",
-                  "aspmysql"));
+                  "rentstuff"));
 
                 // if MigrationHistory table is not there (which is the case first time we run) - create it
                 if (migrationHistoryTableExists.FirstOrDefault() == 0)
