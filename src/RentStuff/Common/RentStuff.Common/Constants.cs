656b5c5c20ac0c83c244a4e20bd249ed949305bc
waqas-syed <waqas.shah.syed@gmail.com>
2017-05-26 17:00:18 +0500

    Removing index.html from the frontend urls

--- src/RentStuff/Common/RentStuff.Common/Constants.cs
+++ src/RentStuff/Common/RentStuff.Common/Constants.cs
@@ -8,8 +8,8 @@ namespace RentStuff.Common
         public const string CompanyName = "Zarqoon";
         //public const string FrontEndUrl = "http://localhost:11803";
         public static readonly string FrontEndUrl = ConfigurationManager.AppSettings.Get("FrontEndUrl");
-        public const string AccountActivationUrlLocation = "index.html#/activate-account";
-        public const string PasswordResetUrlLocation = "index.html#/reset-password";
+        public const string AccountActivationUrlLocation = "activate-account";
+        public const string PasswordResetUrlLocation = "reset-password";
         public const string HOUSEIMAGESDIRECTORY = "~/Images/";
     }
 }
