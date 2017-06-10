dac4abd2f4319606cb937d001ccd5faf511d9d3b
waqas-syed <waqas.shah.syed@gmail.com>
2017-06-11 00:02:15 +0500

    Spring.Net replaced successfully in THe WebHost Project by Ninject

--- src/RentStuff/Common/RentStuff.Common.WebHost/Startup.cs
+++ src/RentStuff/Common/RentStuff.Common.WebHost/Startup.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Reflection;
 using System.Threading.Tasks;
 using System.Web.Cors;
 using System.Web.Http;
@@ -6,9 +7,17 @@ using Microsoft.Owin;
 using Microsoft.Owin.Cors;
 using Microsoft.Owin.Security.DataProtection;
 using Microsoft.Owin.Security.OAuth;
+using Ninject;
+using Ninject.Web.Common.OwinHost;
+using Ninject.Web.WebApi;
+using Ninject.Web.WebApi.OwinHost;
 using NLog;
 using Owin;
+using RentStuff.Common.NinjectModules;
 using RentStuff.Common.WebHost.Providers;
+using RentStuff.Property.Application.Ninject.Modules;
+using RentStuff.Property.Infrastructure.Persistence.Ninject.Modules;
+using RentStuff.Property.Ports.Adapter.Rest.Ninject.Modules;
 
 [assembly: OwinStartup(typeof(RentStuff.Common.WebHost.Startup))]
 namespace RentStuff.Common.WebHost
@@ -53,12 +62,24 @@ namespace RentStuff.Common.WebHost
 
             //app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             ConfigureOAuth(app);
-            WebApiConfig.Register(config);            
-            app.UseWebApi(config);
-            
+            WebApiConfig.Register(config);
+            //app.UseWebApi(config);
+            app.UseNinjectMiddleware(CreateKernel).UseNinjectWebApi(config);
+
             _logger.Info("ASP.NET and OWIN pipeline established");
         }
-        
+
+        private static StandardKernel CreateKernel()
+        {
+            var kernel = new StandardKernel();
+            kernel.Load<CommonNinjectModule>();
+            kernel.Load<PropertyPersistenceNinjectModule>();
+            kernel.Load<PropertyApplicationNinjectModule>();
+            kernel.Load<PropertyPortsNinjectModule>();
+            GlobalConfiguration.Configuration.DependencyResolver = new NinjectDependencyResolver(kernel);
+            return kernel;
+        }
+
         private void ConfigureOAuth(IAppBuilder app)
         {
             OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions()
