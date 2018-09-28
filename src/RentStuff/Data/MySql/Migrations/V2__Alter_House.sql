544e82ab19a5351de6d2fb237a14e01dea03e0a8
waqas-syed <waqas.shah.syed@gmail.com>
2018-09-28 11:45:50 +0500

    Adding the migrations folder inside of the Data/MySql folder

---
+++ src/RentStuff/Data/MySql/Migrations/V2__Alter_House.sql
@@ -0,0 +1,10 @@
+alter table house 
+add column `ac` tinyint(1) DEFAULT NULL,
+add column `geyser` tinyint(1) DEFAULT NULL,
+add column `elevator` tinyint(1) DEFAULT NULL,
+add column `balcony` tinyint(1) DEFAULT NULL,
+add column `lawn` tinyint(1) DEFAULT NULL,
+add column `cctv_cameras` tinyint(1) DEFAULT NULL,
+add column `backup_electricity` tinyint(1) DEFAULT NULL,
+add column `heating` tinyint(1) DEFAULT NULL,
+add column `bathtub` tinyint(1) DEFAULT NULL;
